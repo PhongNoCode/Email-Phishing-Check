10e69db4fc66cde8f6c91e4deaa9d6d6a8382fef
PhongTapCode <nguyenduongphong27062005@gmail.com>
2026-08-12 08:35:27 +0700

    Remove analyze_urgent_headers function
    Removed the analyze_urgent_headers function that used the Aho-Corasick algorithm for detecting urgent headers in emails.

--- core/parsers/parser_body.py
+++ core/parsers/parser_body.py
@@ -1,7 +1,5 @@
 import re
 import email
-import ahocorasick
-from core.email_analyzer.constants import PHISHING_EMAIL_KEYWORDS
 
 def analyze_content(email_file_path):
     """Analyze the email routing and Message-ID.
@@ -60,28 +58,3 @@ def analyze_link_urls(email_file_path):
     return url_risk_scores
 
 
-def analyze_urgent_headers(email_file_path):
-    """Analyze the email content for urgent headers using the Aho-Corasick algorithm.
-
-    Args:
-        email_file_path (str): The file path to the .eml file.
-
-    Returns:
-            dict: A dictionary containing the email urgent headers.
-    """
-    
-    automaton = ahocorasick.Automaton()
-    urgent_headers = {}
-    list_of_urgent_headers = []
-    with open(email_file_path, 'r', encoding='utf-8') as file:
-        haystack = "".join(file.readlines()).lower()
-        for idx, key in enumerate(PHISHING_EMAIL_KEYWORDS):
-            automaton.add_word(key, (idx, key))
-        automaton.make_automaton()
-        for end_index, (insert_order, original_value) in automaton.iter(haystack):
-            start_index = end_index - len(original_value) + 1
-            list_of_urgent_headers.append(original_value)
-            assert haystack[start_index:start_index + len(original_value)] == original_value
-    urgent_headers['urgent_headers'] = list_of_urgent_headers
-
-    return urgent_headers
