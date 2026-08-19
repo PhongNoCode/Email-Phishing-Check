c7afa7939b55bc9978879a72cc49fdb553887e1b
PhongTapCode <nguyenduongphong27062005@gmail.com>
2026-08-19 16:32:07 +0700

    Add new analysis functions to parser module

--- core/parsers/__init__.py
+++ core/parsers/__init__.py
@@ -1,7 +1,7 @@
 from .parser_utils import extract_domain
 from .parser_headers import analyze_header, analyze_route, analyze_subject, analyze_urgent_headers
-from .parser_body import analyze_body_content, analyze_link_urls, analyze_urgent_body_content
-from .scanner_attachments import analyze_attachment, analyze_extension, analyze_email_macros, analyze_pdf
+from .parser_body import analyze_body_content, analyze_link_urls, analyze_urgent_body_content, analyze_potential_passwords
+from .scanner_attachments import analyze_attachment, analyze_extension, analyze_email_macros, analyze_pdf, analyze_universal_extract
 from .threat_intel import analyze_homoglyph, analyze_typo
 
 __all__ = [
@@ -18,5 +18,8 @@ __all__ = [
     'analyze_typo',
     'analyze_urgent_body_content',
     'analyze_body_content',
-    'analyze_pdf'
+    'analyze_pdf',
+    'analyze_potential_passwords',
+    'analyze_universal_extract'
+
 ]
