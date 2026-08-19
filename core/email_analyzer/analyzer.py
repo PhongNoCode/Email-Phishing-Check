a5615da5e2dbc91988d1cd75544d086306b4ee0e
PhongTapCode <nguyenduongphong27062005@gmail.com>
2026-08-19 16:31:38 +0700

    Refactor StaticAnalyzer for email message handling
    Refactor StaticAnalyzer to use email.message_from_binary_file and update analysis methods to handle email_message object.

--- core/email_analyzer/analyzer.py
+++ core/email_analyzer/analyzer.py
@@ -2,35 +2,52 @@
 
 import concurrent.futures
 from rich.console import Console
-
+import email
 from core.email_analyzer.modules import authentication
 from core.email_analyzer.modules import attachment
 from core.email_analyzer.modules import identity
 from core.email_analyzer.modules import url
 from core.email_analyzer.modules import body
 from core.email_analyzer.modules import header as header_module
+from core.ast_analyzer.main_ast import analyze_python_files
 from core.parsers import *
 console = Console()
 
 class StaticAnalyzer:
     """Class to perform static analysis on an email file."""
 
-    def __init__(self, email_file):
+    def __init__(self, email_file_path):
         """Initialize the analysis object for a specific email file."""
-        self.email = email_file
-        self.header = analyze_header(email_file)
-        self.route = analyze_route(email_file)
-        self.ext = analyze_extension(email_file)
-        self.url = analyze_link_urls(email_file)
-        self.subject = analyze_subject(email_file)
-        self.hash_of_file = analyze_attachment(email_file)
-        self.urgent_headers = analyze_urgent_headers(email_file)
-        self.macro_analysis = analyze_email_macros(email_file)
-        self.body_content = analyze_body_content(email_file)
-        self.pdf = analyze_pdf(email_file)
+        self.email_file_path = email_file_path
+        
+
+        with open(email_file_path, 'rb') as f:
+            self.email_message = email.message_from_binary_file(f)
+            
+        
+        raw_email_text = self.email_message.as_string().lower()
+
+        
+        self.header = analyze_header(self.email_message)
+        self.route = analyze_route(self.email_message)
+        self.subject = analyze_subject(self.email_message)
+        self.urgent_headers = analyze_urgent_headers(raw_email_text)
+        passwords = analyze_potential_passwords(raw_email_text)
+        self.extracted_files = analyze_universal_extract(self.email_message, passwords)
+        
+        self.ext = analyze_extension(self.extracted_files) 
+        self.url = analyze_link_urls(raw_email_text)
+        self.hash_of_file = analyze_attachment(self.extracted_files)
+        self.macro_analysis = analyze_email_macros(self.extracted_files)
+        self.pdf = analyze_pdf(self.extracted_files)
+
+        self.body_content = analyze_body_content(self.email_message)
         self.urgent_body_content = analyze_urgent_body_content(self.body_content)
         self.homoglyph = analyze_homoglyph(self.header, self.url)
         self.typo = analyze_typo(self.header, self.url)
+
+        self.ast_analysis = analyze_python_files(self.extracted_files)
+        
         self.total_score = 0
 
     # Header
@@ -74,7 +91,7 @@ class StaticAnalyzer:
         return identity.check_typo(self.typo)
 
     def check_body_content(self):
-        return body.check_urgent_body(self.body_content)
+        return body.check_urgent_body(self.urgent_body_content)
 
     def run_all(self):  
         """Execute all checks concurrently using a ThreadPoolExecutor."""
